2c0816e5b9d4b38f9a3ac73d44874a40cfedd5a3
Patsch David (patc) <patc@zhaw.ch>
2022-03-22 16:14:01 +0100

    small changes

--- config.py
+++ config.py
@@ -1,8 +1,13 @@
 """
-set up remote hosts with a dictionary where the key is your remote address/alias and the value is the name you want displayed
+set up remote hosts with a dictionary where the key is your remote address and then additional configuratins in a nested dictionary
+the alias describes the displayed name for your server, num_cpu the amount of cpus that can be used (0 => disabled) and then the port you 
+connect to the docker container on. 77 is the default. dont change this unless you have to (then you have to expose a different port in your docker too)
 hosts={
-  "localhost": "local",  # this will connect to localhost, and display "local" in the list of remotes
-  "remote1": "server"
+  "localhost": { # this will connect to localhost, and display "local" in the list of remotes
+    "alias": "local" #the gui will display your localhost as local in the list of hosts
+    "num_cpu": 64 #docking runs will be allowed 64 cpu cores
+    "connect_on_port": 77 # by default the port 77 will connect to 22 (ssh default) on the docker 
+  },  
 }
 
 """
@@ -18,7 +23,6 @@ HOSTS = {
         "num_cpu": 16,
     },
 }
-
 
 # specify a link you want the map icon to link to. help_path="link"
 HELP_PATH = "https://biocatalysis.wiki.zhaw.ch/software/autodock_for_pymol"
--- main.py
+++ main.py
@@ -6,12 +6,10 @@ import os
 import sys
 
 sys.path.insert(1, os.path.dirname(__file__))
-print(os.path.dirname(__file__), "DIRNAME")
 
-from PyQt5.QtWidgets import QApplication
 from alphadock.alphadock_main_wndw import dock_gui
+from PyQt5.QtWidgets import QApplication
 
-print("attempting to import from")
 
 
 def __init_plugin__(app=None):
